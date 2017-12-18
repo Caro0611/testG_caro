86b2d51657869d39b05f25483107ae84236784cb
Mendez Acosta <a_mendezacosta@tss-hr420-dm23.fc.ca>
2017-12-18 18:03:06 -0500

    changing modules2

---
+++ js/modules/module2.js
@@ -0,0 +1,10 @@
+myApp.module1= {
+  saySomething(message){
+    console.log(myApp.mainMessage, message, "i am the secong file");
+
+  }
+
+  doSomething(){
+    console.log('called module1 doSomething function')
+  }
+}
